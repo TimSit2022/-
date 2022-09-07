150120ca09602fd60e95a417b147cd16e2c160d2
student <student@C251U34>
2022-09-07 11:39:52 +0300

    Добавьте файлы проекта.

---
+++ Первое занятие/Первое занятие.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>      //For I/O Functions
+#include <windows.h>   //For Beep & Sleep functions
+#include<stdlib.h>  //For System Pause
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 30; i++)
+        Beep(400 + i * 50, 1000 - i * 40);
+}
